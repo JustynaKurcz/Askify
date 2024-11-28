e05fc1f5c38f198c0ca8982b16de2cedf1e26e04
Justyna <justyna.kurcz1@gmail.com>
2024-11-28 17:14:45 +0100

    quick fix

---
+++ AskifyAPI/src/Askify.Infrastructure/EF/Users/Queries/GetCurrentLoggedUser/GetCurrentLoggedUserQueryHandler.cs
@@ -0,0 +1,29 @@
+using Askify.Application.Users.Queries.DTO;
+using Askify.Application.Users.Queries.GetCurrentLoggedUser;
+using Askify.Core.Users.Errors;
+using Askify.Core.Users.Repositories;
+using Askify.Shared.Auth.Context;
+using Askify.Shared.Results;
+using MediatR;
+
+namespace Askify.Infrastructure.EF.Users.Queries.GetCurrentLoggedUser;
+
+internal sealed class GetCurrentLoggedUserQueryHandler(
+    IContext context,
+    IUserRepository userRepository
+) : IRequestHandler<GetCurrentLoggedUserQuery, Result<UserDetailsDto, Error>>
+{
+    public async Task<Result<UserDetailsDto, Error>> Handle(GetCurrentLoggedUserQuery query,
+        CancellationToken cancellationToken)
+    {
+        var userId = context.Identity.Id;
+        var user = await userRepository.GetAsync(userId, cancellationToken);
+
+        if (user is null)
+        {
+            return UserError.UserNotFound(userId);
+        }
+        
+        return user.AsDetailsDto();
+    }
+}
