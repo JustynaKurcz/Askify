fa283d8abc6b05aa0451e951976cf5c982a68985
Justyna <justyna.kurcz1@gmail.com>
2025-01-24 18:15:11 +0100

    Client: add footer

--- AskifyClient/src/app/app.component.ts
+++ AskifyClient/src/app/app.component.ts
@@ -4,12 +4,13 @@ import {NavbarComponent} from './components/navbar/navbar.component';
 import {ToastModule} from 'primeng/toast';
 import {MessageService} from 'primeng/api';
 import {CommonModule} from '@angular/common';
+import {FooterComponent} from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [CommonModule, RouterOutlet, RouterModule, ToastModule, NavbarComponent],
+  imports: [CommonModule, RouterOutlet, RouterModule, ToastModule, NavbarComponent, FooterComponent],
   providers: [MessageService],
   styleUrl: './app.component.css'
 })
